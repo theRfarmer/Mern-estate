b4361cf4e27b952b59ebc9902ad04c1646362906
Akshayy <patelsinghakshay30@gmail.com>
2024-07-09 07:19:03 +0530

    "error handling and auth"

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import UserRouter from './routes/user.route.js'
-
+import authRouter from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -16,9 +16,19 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 
 
 const app =express();
+app.use(express.json())
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!!')
 })
 
 app.use('/api/user',UserRouter)
+app.use('/api/auth',authRouter)
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode||500;
+    const message=err.message||'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+    statusCode,message,    })
+})
