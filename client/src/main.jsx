6315d87aebe10954cf2f915632d24c14e0ad869a
Akshayy <patelsinghakshay30@gmail.com>
2024-07-10 20:55:29 +0530

    "added redux and redux persistor"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import AppLayout from './App';
+import { persistor, store } from './redux/store.js';
+import {Provider} from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <AppLayout />
-  </React.StrictMode>
+    </PersistGate>
+  </Provider>
+  
 );
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import {Link,useNavigate} from "react-router-dom"
-
+import { useDispatch, useSelector } from 'react-redux';
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData,setFormData]=useState({});
-  const [error,setError]=useState(null);
-  const [loading,setLoading]=useState(false);
+  const {error,loading}=useSelector((state)=>state.user);
+
 
 const navigate=useNavigate();
-
+const dispatch= useDispatch();
 const handleChange=(e)=>{
 setFormData({
   ...formData,
@@ -18,7 +19,7 @@ setFormData({
 const handleSubmit = async (e)=>{
   e.preventDefault();
   try{
-    setLoading(true);
+   dispatch(signInStart())
     const res=await fetch('/api/auth/signin',
   {
     method:'POST',
@@ -29,17 +30,14 @@ const handleSubmit = async (e)=>{
   });
   const data= await res.json();
   if(data.success=== false){
-    setError(data.message);
-    setLoading(false)
+    dispatch(signInFailure(data.message))
     return;
   }
-  setLoading(false);
-  setError(null);
+  dispatch(signInSuccess(data))
   navigate('/');
   }
 catch(error){
-setLoading(false);
-setError(error.message);
+dispatch(signInFailure(error.message));
 }
  
 };
